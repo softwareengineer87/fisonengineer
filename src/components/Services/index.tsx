8f4db8c8532b28c9fa688a3a99339560b6c4bc03
Software Engineer <alex.vicenteofficial@gmail.com>
2025-11-01 21:36:58 -0300

    feat: Create services section

--- src/components/Services/index.tsx
+++ src/components/Services/index.tsx
@@ -5,42 +5,39 @@ import imageService from '../../../public/image-services.jpg';
 
 function Services() {
   return (
-    <section className='container-services' id='services'>
+    <section className='container-services' id='servicos'>
       <h3>Serviços</h3>
       <div className='services container'>
         <div className='services-left'>
           <div className='service'>
             <h4>Desenvolvimento de sites</h4>
             <p>
-              Sites que impressionam, convertem e fortalecem sua marca.  
-          Criamos experiências digitais sob medida, com design moderno, 
-                    performance impecável e foco em resultados reais.
-              >
-            v>
-
-          <div className='service'>
-            <h4>Desenvolvimento de sistemas</h4>
-            <p>
-              Sistemas inteligentes, ágeis e feitos sob medida
-              para o seu negócio.  Automatize processos, ganhe 
-              odutividade  tenha o controle total da sua operação — tudo online.
+              Sites que impressionam, convertem e fortalecem sua marca. 
+            Criamos experiências digitais sob medida, com design moderno, 
+               performance impecável e foco em resultados reais.
             </p>
           </div>
-        </div>
-
+            <div className='service'>
+              <h4>Desenvolvimento de sistemas</h4>
+              <p>
+                Sistemas inteligentes, ágeis e feitos sob medida
+                para o seu negócio. Automatize processos, 
+              ganhe prdutividade e tenha o controle total da sua operação — tudo online.
+              </p>
+            </div>
+          </div>
         <div className='services-right'>
-         <Image
+      <Image
         src={imageService}
-          idth={500}
-            height={750}
-            alt='Imagem profissional trabalhando'
-                 />
-                </div>
-          
-        ion>
+        width={500}
+        height={750}
+        alt='Profissional trabalhando'
+      /> 
+      </div>
+    </div> 
+ </section>
+  );
+} 
 
-
-    
-
-eport { Services }
+export { Services }
 
--- src/components/Sobre/index.tsx
+++ src/components/Sobre/index.tsx
@@ -1,5 +1,4 @@
 
-import Image from 'next/image';
 import styles from './sobre.module.css';
 
 function Sobre() {
